775da6803db79ac8366f4d5aa7fc362ffd3b7b48
Gianmarco G <giangarof98@gmail.com>
2022-05-20 10:56:25 -0400

    randomId() with crypto

--- repositories/user.js
+++ repositories/user.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
     constructor(filename){
@@ -21,6 +22,8 @@ class UsersRepository {
     }
 
     async create(attrs){
+        attrs.id =this.randomId();
+        
         const records = await this.getAll()
         records.push(attrs);
         await this.writeAll(records)
@@ -28,6 +31,10 @@ class UsersRepository {
 
     async writeAll(records){
         await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
+    }
+
+    randomId(){
+        return crypto.randomBytes(4).toString('hex');
     }
 }
 const test = async () => {
