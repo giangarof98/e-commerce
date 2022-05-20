f3160ac6540d5a289cf339e4e9a2d26c1059f538
Gianmarco G <giangarof98@gmail.com>
2022-05-19 21:04:59 -0400

    reusable bodyParser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req,res) => {
     res.send(`
         <div> 
@@ -13,24 +15,24 @@ app.get('/', (req,res) => {
         </div>`)
 })
 
-const bodyParser = (req, res, next) => {
-    if(req.method === 'POST'){
-        req.on('data', data => {
-            const parsed = data.toString('utf8').split('&');
-            const formData = {};
-            for(let pair of parsed){
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        })
-    } else {
-        next()
-    }
-}
+// const bodyParser = (req, res, next) => {
+//     if(req.method === 'POST'){
+//         req.on('data', data => {
+//             const parsed = data.toString('utf8').split('&');
+//             const formData = {};
+//             for(let pair of parsed){
+//                 const [key, value] = pair.split('=');
+//                 formData[key] = value;
+//             }
+//             req.body = formData;
+//             next();
+//         })
+//     } else {
+//         next()
+//     }
+// }
 
-app.post('/', bodyParser, (req,res) => {
+app.post('/', (req,res) => {
     console.log(req.body)
     res.send(`<div>created</div>`)
     
