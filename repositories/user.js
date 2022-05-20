803e2e1876b106f94a7cb55fea168efff55bd22a
Gianmarco G <giangarof98@gmail.com>
2022-05-20 12:11:27 -0400

    update() by id

--- repositories/user.js
+++ repositories/user.js
@@ -48,6 +48,18 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords)
     }
+
+    async update(id, attrs){
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if(!record){
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record,attrs);
+        await this.writeAll(records);
+    }
 }
 const test = async () => {
 
@@ -56,7 +68,7 @@ const test = async () => {
     //const users = await repo.getAll();
     //const user = await repo.getOne('3c0');
     //console.log(user);
-    await repo.delete('715af91f')
+    await repo.update('d41a4e8f', {password: 'updated'})
 
 }
 test()
