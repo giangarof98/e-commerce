7aafb5f172482d4abfd19cc14fd8a5e85c203b87
Gianmarco G <giangarof98@gmail.com>
2022-05-20 10:26:53 -0400

    getAll()

--- repositories/user.js
+++ repositories/user.js
@@ -13,7 +13,18 @@ class UsersRepository {
         }
     }
 
-    async checkForFile(){}
+    async getAll(){
+        //open file this.filename
+        return JSON.parse(await fs.promises.readFile(this.filename, {
+            encoding: 'utf8'
+        }));
+    }
 }
+const test = async () => {
 
-const repo = new UsersRepository('users.json');
+    const repo = new UsersRepository('users.json');
+    const users = await repo.getAll();
+    console.log(users);
+
+}
+test()
