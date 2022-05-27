aaa44799d905bc9b956f65c8db244ceccb6348bb
Gianmarco G <giangarof98@gmail.com>
2022-05-27 15:14:51 -0400

    products validation

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,14 +1,23 @@
 const express = require('express');
-
+const {validationResult} = require('express-validator');
+const productsRepo = require('../../repositories/products')
+const productsNewTemplate = require('../../views/admin/products/new')
+const {requirePrice, requireTitle} = require('./validators')
 const router = express.Router();
 
-router.get('/admin/products', (req,res) => {
-
+router.get('/admin/products',  (req,res) => {
+    
 });
 
 router.get('/admin/products/new', (req,res) => {
-
+    res.send(productsNewTemplate({}));
 });
+
+router.post('/admin/products/new', [requirePrice, requireTitle], (req,res) => {
+    const errors = validationResult(req);
+    console.log(errors)
+    res.send('submited')
+})
 
 
 
