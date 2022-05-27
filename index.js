65a79f6bb4ff34959b7837770748b93f4cca2560
Gianmarco G <giangarof98@gmail.com>
2022-05-27 13:53:00 -0400

    repository for products. Reusable class for products and users

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productRouter = require('./routes/admin/products');
 const app = express();
+
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}));
 app.use(cookieSession({
     keys: ['secret']
 }));
 app.use(authRouter);
+app.use(productRouter);
 
 const port = 3000;
 app.listen(port, () => {
