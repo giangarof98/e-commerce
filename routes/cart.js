3c928afa692737dc09e22be8a25639cc2612ee48
Gianmarco G <giangarof98@gmail.com>
2022-05-31 22:37:05 -0400

    delete item from cart

--- routes/cart.js
+++ routes/cart.js
@@ -21,7 +21,7 @@ router.post('/cart/products', async(req,res) => {
     await cartRepo.update(cart.id, {
         items: cart.items
     });
-    res.send('added to cart')
+    res.redirect('/cart')
 });
 
 router.get('/cart', async (req,res) => {
@@ -36,6 +36,14 @@ router.get('/cart', async (req,res) => {
         item.product = product;
     }
     res.send(cartShowTemplate({items: cart.items}));
-})
+});
+
+router.post('/cart/products/delete', async(req, res) => {
+    const {itemId} = req.body;
+    const cart = await cartRepo.getOne(req.session.cartId);
+    const items = cart.items.filter(item => item.id !== itemId);
+    await cartRepo.update(req.session.cartId, {items});
+    res.redirect('/cart')
+});
 
 module.exports = router;
